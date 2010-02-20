4b744cc406b5d1d8c7d225c3e57a63b682216128
badlogicgames <badlogicgames@da89c470-1dbd-11df-9135-9f0b1fc706c8>
2010-02-20 22:22:31 +0000

    [added] real-time capabilities for Plot. adapted examples

--- src/com/badlogic/audio/samples/BandedSpectralFlux.java
+++ src/com/badlogic/audio/samples/BandedSpectralFlux.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import com.badlogic.audio.analysis.FFT;
+import com.badlogic.audio.io.AudioDevice;
 import com.badlogic.audio.io.MP3Decoder;
 import com.badlogic.audio.visualization.Plot;
 
@@ -15,17 +16,20 @@ import com.badlogic.audio.visualization.Plot;
  * @author mzechner
  *
  */
-public class BandedSpectralFlux {
+public class BandedSpectralFlux 
+{
+	public static final String FILE = "samples/sample.mp3";
+	
 	public static void main( String[] argv ) throws FileNotFoundException, Exception
 	{
-		MP3Decoder decoder = new MP3Decoder( new FileInputStream( "samples/cochise.mp3" ) );
+		MP3Decoder decoder = new MP3Decoder( new FileInputStream( FILE ) );
 		float samples[] = new float[1024];
 		float lastSpectrum[] = new float[513];
 		ArrayList<Float> spectralFlux = new ArrayList<Float>( );
 		FFT fft = new FFT( 1024, 44100 );
 		fft.window( FFT.HAMMING );
-		int startFrequency = fft.freqToIndex( 10000 );
-		int endFrequency = fft.freqToIndex( 14000 );
+		int startFrequency = fft.freqToIndex( 0 );
+		int endFrequency = fft.freqToIndex( 4000 );
 		
 		while( decoder.readSamples( samples ) > 0 )
 		{
@@ -44,7 +48,26 @@ public class BandedSpectralFlux {
 		float[] spectralFluxArray = new float[spectralFlux.size()];
 		for( int i = 0; i < spectralFlux.size(); i++ )
 			spectralFluxArray[i] = spectralFlux.get(i);
-		Plot plot = new Plot( "Spectral Flux", 512, 512);
+		Plot plot = new Plot( "Spectral Flux", 1024, 512);
 		plot.plot( spectralFluxArray, 1, Color.red );
+		
+		playBack( plot );
+	}
+	
+	public static void playBack( Plot plot ) throws Exception
+	{
+		MP3Decoder decoder = new MP3Decoder( new FileInputStream( FILE ) );
+		AudioDevice device = new AudioDevice( );
+		float samples[] = new float[1024];
+		
+		long startTime = System.nanoTime();
+		while( decoder.readSamples( samples ) > 0 )
+		{
+			device.writeSamples( samples );
+			float elapsedTime = (System.nanoTime()-startTime)/1000000000.0f;
+			int position = (int)(elapsedTime * (44100/1024)); 
+			plot.setMarker( position, Color.white );			
+			Thread.sleep(20); 
+		}
 	}
 }
--- src/com/badlogic/audio/samples/RealTimePlot.java
+++ src/com/badlogic/audio/samples/RealTimePlot.java
@@ -7,14 +7,55 @@ import java.util.ArrayList;
 
 import com.badlogic.audio.io.AudioDevice;
 import com.badlogic.audio.io.MP3Decoder;
-import com.badlogic.audio.io.WaveDecoder;
 import com.badlogic.audio.visualization.Plot;
 
+/**
+ * A simple example on how to do real-time plotting. First all samples
+ * from an mp3 file are read in and plotted, 1024 samples per pixel. Next
+ * we open a new MP3Decoder and an AudioDevice and play back the music. While
+ * doing this we also set a marker in the plot that shows us the current position
+ * of the music playback. The marker position is calculated in pixels by
+ * measuring the elapsed time between the start of the playback and the 
+ * current time. The elapsed time is then multiplied by the frequency divided
+ * by the sample window size (1024 samples in this case). This gives us the
+ * x-coordinate of the marker in the plot. After writting a sample window
+ * to the audio device and setting the marker we sleep for 20ms to give
+ * the Swing GUI thread time to repaint the plot with the updated marker
+ * position.
+ * 
+ * @author mzechner
+ *
+ */
 public class RealTimePlot 
 {
+	private static final int SAMPLE_WINDOW_SIZE = 1024;	
+	private static final String FILE = "samples/mozart.mp3";
+	
 	public static void main( String[] argv ) throws FileNotFoundException, Exception
 	{
-		MP3Decoder decoder = new MP3Decoder( new FileInputStream( "samples/cochise.mp3" ) );
+		float[] samples = readInAllSamples( FILE );
+
+		Plot plot = new Plot( "Wave Plot", 512, 512 );
+		plot.plot( samples, SAMPLE_WINDOW_SIZE, Color.red );		
+		
+		MP3Decoder decoder = new MP3Decoder( new FileInputStream( FILE ) );
+		AudioDevice device = new AudioDevice( );
+		samples = new float[SAMPLE_WINDOW_SIZE];
+		
+		long startTime = System.nanoTime();
+		while( decoder.readSamples( samples ) > 0 )
+		{
+			device.writeSamples( samples );
+			float elapsedTime = (System.nanoTime()-startTime)/1000000000.0f;
+			int position = (int)(elapsedTime * (44100/SAMPLE_WINDOW_SIZE)); 
+			plot.setMarker( position, Color.white );			
+			Thread.sleep(20); // this is needed or else swing has no chance repainting the plot!
+		}
+	}
+	
+	public static float[] readInAllSamples( String file ) throws FileNotFoundException, Exception
+	{
+		MP3Decoder decoder = new MP3Decoder( new FileInputStream( file ) );
 		ArrayList<Float> allSamples = new ArrayList<Float>( );
 		float[] samples = new float[1024];
 
@@ -28,21 +69,6 @@ public class RealTimePlot
 		for( int i = 0; i < samples.length; i++ )
 			samples[i] = allSamples.get(i);
 
-		Plot plot = new Plot( "Wave Plot", 512, 512 );
-		plot.plot( samples, 44100 / 100, Color.red );
-		allSamples = null; // let'S preserve some memory...
-		
-		decoder = new MP3Decoder( new FileInputStream( "samples/cochise.mp3" ) );
-		AudioDevice device = new AudioDevice( );
-		samples = new float[1024];
-		
-		int windowCount = 0;
-		while( decoder.readSamples( samples ) > 0 )
-		{
-			device.writeSamples( samples );
-			int x = (int)(( 1024.0f / (44100 / 100) ) * windowCount); 
-			plot.drawLine( x, 0, x, 512, Color.white );
-			windowCount++;
-		}
+		return samples;
 	}
 }
