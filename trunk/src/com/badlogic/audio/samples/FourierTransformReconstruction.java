16ba3d9a6915dd0be7bf974ada3d85d44e23405c
badlogicgames <badlogicgames@da89c470-1dbd-11df-9135-9f0b1fc706c8>
2010-02-20 02:15:12 +0000

    [added] fourier transform from minim project plus example

---
+++ trunk/src/com/badlogic/audio/samples/FourierTransformReconstruction.java
@@ -0,0 +1,32 @@
+package com.badlogic.audio.samples;
+
+import java.io.FileInputStream;
+
+import com.badlogic.audio.analysis.FFT;
+import com.badlogic.audio.io.AudioDevice;
+import com.badlogic.audio.io.WaveDecoder;
+
+/**
+ * A simple example that shows that transforming samples to
+ * the frequency domain and back to the time domain preserves
+ * the original signal nearly perfectly.
+ * @author mzechner
+ *
+ */
+public class FourierTransformReconstruction 
+{
+	public static void main( String[] argv ) throws Exception
+	{
+		AudioDevice device = new AudioDevice( );
+		WaveDecoder decoder = new WaveDecoder( new FileInputStream( "samples/sample.wav" ) );
+		float[] samples = new float[1024];
+		FFT fft = new FFT( 1024, 44100 );
+		
+		while( decoder.readSamples( samples ) > 0 )
+		{
+			fft.forward( samples );
+			fft.inverse( samples );
+			device.writeSamples( samples );
+		}
+	}
+}
