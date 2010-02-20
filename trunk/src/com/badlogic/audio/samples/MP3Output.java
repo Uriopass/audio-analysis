7ea798762320d189f541b4168643e6b470642b2a
badlogicgames <badlogicgames@da89c470-1dbd-11df-9135-9f0b1fc706c8>
2010-02-20 15:12:54 +0000

    [added] MP3Decoder and example

---
+++ trunk/src/com/badlogic/audio/samples/MP3Output.java
@@ -0,0 +1,29 @@
+package com.badlogic.audio.samples;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.badlogic.audio.io.AudioDevice;
+import com.badlogic.audio.io.MP3Decoder;
+
+/**
+ * Simple example that shows how to decode an mp3 file.
+ * 
+ * @author mzechner
+ *
+ */
+public class MP3Output 
+{
+	public static void main( String[] argv ) throws FileNotFoundException, Exception
+	{
+		AudioDevice device = new AudioDevice( );
+		MP3Decoder decoder = new MP3Decoder( new FileInputStream( "samples/sample.mp3" ) );
+		float[] samples = new float[1024];
+		
+		while( decoder.readSamples( samples ) > 0 )
+		{
+			device.writeSamples( samples );
+//			System.out.println( "samples" );
+		}
+	}
+}
